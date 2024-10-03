1054e9f8ab9d2d7b8a5333732b50feb0cd2584ef
Suzal <sujalmdhr10@gmail.com>
2024-10-03 10:23:30 +0545

    Second Commit

---
+++ gain.java
@@ -0,0 +1,7 @@
+public class gain {
+    public static void main(String[] args) {
+        System.out.println("lauda jasto gain");
+        System.out.println("AgAIN HELLO WORLD");
+    }
+    
+}
--- main.java
+++
@@ -1,8 +0,0 @@
-public class main{
-    public static void main(String[] args) {
-        
-        System.out.println("hello world");
-        System.out.println("checking on with jenkins");
-        System.out.println("Second commit to reflect change");
-    }
-}
